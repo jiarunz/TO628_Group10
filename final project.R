278ef964d7633fb807711fa6c057c739e5665a18
jiarunz <62670767+jiarunz@users.noreply.github.com>
2020-04-16 16:38:12 -0400

    Update final project.R

--- final project.R
+++ final project.R
@@ -1,7 +1,4 @@
-#read data
 airbnb <- read.csv("AB_NYC_2019.csv")
-str(airbnb)
-View(airbnb)
 
 #Clean data
 airbnb$id <- NULL
@@ -10,31 +7,32 @@ airbnb$host_id <- NULL
 airbnb$host_name <- NULL
 airbnb$latitude <- NULL
 airbnb$longitude <- NULL
-# str(airbnb)
-airbnb$reviews_per_month<- ifelse(is.na(airbnb$reviews_per_month), mean(airbnb$reviews_per_month, na.rm = TRUE), airbnb$reviews_per_month)
 airbnb$neighbourhood_group <- as.factor(airbnb$neighbourhood_group)
 airbnb$neighbourhood <- as.factor(airbnb$neighbourhood)
 airbnb$room_type <- as.factor(airbnb$room_type)
+
+airbnb$reviews_per_month<- ifelse(is.na(airbnb$reviews_per_month), mean(airbnb$reviews_per_month, na.rm = TRUE), airbnb$reviews_per_month)
+last_half_year_rw <- ifelse(as.Date(airbnb$last_review)>as.Date("2019-01-07"),1,0)
+last_half_year_rw <- ifelse(is.na(last_half_year_rw),0,last_half_year_rw)
+airbnb$last_review <- last_half_year_rw
+airbnb$last_review <- as.factor(airbnb$last_review)
 
 #Pick data
 manhattan <- airbnb[airbnb$neighbourhood_group=="Manhattan",]
 table(manhattan$neighbourhood)
 manhattan_five <- names(table(manhattan$neighbourhood)[order(-table(manhattan$neighbourhood))][1:5])
+manhattan_five
 airbnb <- subset(airbnb,airbnb$neighbourhood==manhattan_five)
-
-last_half_year_rw <- ifelse(as.Date(airbnb$last_review)>as.Date("2019-01-07"),1,0)
-last_half_year_rw <- ifelse(is.na(last_half_year_rw),0,last_half_year_rw)
-airbnb$last_review <- last_half_year_rw
-airbnb$last_review <- as.factor(airbnb$last_review)
-airbnb$neighbourhood <- as.factor(airbnb$neighbourhood)
-airbnb$room_type <- as.factor(airbnb$room_type)
-# summary(last_half_year_rw)
-# str(last_half_year_rw)
-# Drop levels
 airbnb$neighbourhood_group <- droplevels(airbnb$neighbourhood_group)
+airbnb$neighbourhood <- droplevels(airbnb$neighbourhood)
 
 str(airbnb)
 View(airbnb)
+
+#GGplot for price
+library(ggplot2)
+price_plot <- ggplot(airbnb, aes(x=price)) + geom_histogram(fill="red")
+price_plot
 
 # Split Data into Training and Testing in R 
 train =airbnb[1:1695,]
